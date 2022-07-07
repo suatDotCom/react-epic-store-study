a31a05bb94ca003be735cf58b0485fc73aae4c3f
Suat Özçelik (ETIYA) <suat.ozcelik@etiya.com>
2022-07-07 09:02:52 +0300

    - remove unused variables, - memory leak problem fixed

--- src/shared/hooks/useFetchGames.ts
+++ src/shared/hooks/useFetchGames.ts
@@ -2,46 +2,57 @@ import { useState, useEffect, useContext, useCallback } from "react";
 import { IGame } from "../../models/epic-games";
 import { GameService } from "../../services/game.service";
 import { Context } from "../../store";
-import { GET_FAVORITE_GAMES, SET_UNIQUE_CATEGORIES, SET_GAMES } from "../../store/types/game.type";
+import {
+  GET_FAVORITE_GAMES,
+  SET_UNIQUE_CATEGORIES,
+  SET_GAMES,
+} from "../../store/types/game.type";
+import { getGames } from "../storage/GameStorage";
 
 const useFetchGames = () => {
   //@ts-ignore
   const [state, dispatch] = useContext(Context);
   const [data, setData] = useState<any>(null);
 
-  const getCategories = (data: IGame[]): string[] => {
+  const getUniqueCategories = (data: IGame[]): string[] => {
     let uniqueCategories: string[] = [];
     let categories = data.map((item: IGame) => {
       uniqueCategories.push(...item.Categories);
     });
 
     //@ts-ignore
-    return uniqueCategories = [...new Set(uniqueCategories)];
-  }
+    return (uniqueCategories = [...new Set(uniqueCategories)]);
+  };
+
+  useEffect(() => {
+    if (!data) return;
+
+    dispatch({
+      type: SET_GAMES,
+      payload: data,
+    });
+
+    dispatch({
+      type: GET_FAVORITE_GAMES,
+    });
+
+    dispatch({
+      type: SET_UNIQUE_CATEGORIES,
+      payload: getUniqueCategories(data),
+    });
+  }, [data]);
 
   useEffect(
     useCallback(() => {
       GameService.getAll()
         .then((response: Response) => response.json())
-        .then((data: IGame[]) => {
-          setData(data);
-          
-          dispatch({
-            type: SET_GAMES,
-            payload: data,
-          });
-
-          dispatch({
-            type: GET_FAVORITE_GAMES,
-          });
-
-          dispatch({
-            type: SET_UNIQUE_CATEGORIES,
-            payload: getCategories(data)
-          });
+        .then((response: IGame[]) => {
+          var games: IGame[] = getGames();
+          setData(games?.length > 0 ? games : response)
         })
         .catch((error) => {});
-    }, [data])
+    }, []),
+    []
   );
 
   return [data];
